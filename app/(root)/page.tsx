0e2c9af10d5a4776625e2edfecbcb2dd25b4c656
Rainer Nsa <woldreamz@gmail.com>
2024-09-30 19:58:40 +0100

    Files Initialized

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+
+const Home = () => {
+  return (
+    <div className="flex justify-center items-center">
+      <Button>Click Me</Button>
+    </div>
+  );
+};
+export default Home;
